f4155b87d8962e2a35cd8ea005b5de59059480f5
Ajoad Islam Nishat <ajoadislam@gmail.com>
2025-08-06 21:44:07 +0600

    hamburger menu + Candidates Profile Preview

--- client/components/workspace/menu/RecruitersWorkSpaceMenu.tsx
+++ client/components/workspace/menu/RecruitersWorkSpaceMenu.tsx
@@ -15,6 +15,7 @@ import pendingLogo from '../../../public/pending.svg'
 import interviewLogo from '../../../public/interview.svg'
 import expiredLogo from '../../../public/ban.svg'
 import createLogo from '../../../public/create.svg'
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 const majorMono = Major_Mono_Display({ weight: '400', subsets: ['latin'] });
 
@@ -27,11 +28,16 @@ const RecruitersWorkSpaceMenu = () => {
       <div className="w-full h-auto flex flex-col justify-start">
         <Link href={'/'} className={`${majorMono.className} text-2xl`}>EVALIA</Link>
         <div className="w-full h-auto mt-[20px] flex flex-col justify-start items-start pl-[10px] text-gray-400 font-semibold">
-          <button className="flex justify-start items-center gap-1" onClick={()=>setIsShowJobCategory((prev)=>!prev)}>
-            <Image src={jobLogo} alt="coursesLogo" className="h-[15px] w-auto"/>
-            <p className="hover:text-gray-300">Jobs</p>
+          <button className="flex justify-between pl-2 pr-3 rounded-sm py-1 w-[60%] h-full cursor-pointer group  items-center gap-1 mb-2" onClick={()=>setIsShowJobCategory((prev)=>!prev)}>
+            <div className="w-auto h-full flex justify-start items-center gap-1">
+                <Image src={jobLogo} alt="coursesLogo" className="h-[15px] w-auto"/>
+                <p className="group-hover:text-gray-300">Jobs</p>
+            </div>
+            {
+              isShowJobCategory?<ChevronUp className="w-[20px group-hover:text-white"/>:<ChevronDown className="w-[20px group-hover:text-white"/>
+            }
           </button>
-          <ul className={`pl-4 ${isShowJobCategory?'flex flex-col':'hidden'}  gap-1`}>
+          <ul className={`pl-10 ${isShowJobCategory?'flex flex-col':'hidden'}  gap-1`}>
             <Link href={'/workspace/jobs/my-jobs'} className="flex justify-start items-center gap-1 hover:text-gray-300">
               <Image src={allLogo} alt="all" className="h-[13px] w-auto"/>
               <p className="text-sm  cursor-pointer">My Jobs</p>
@@ -40,28 +46,6 @@ const RecruitersWorkSpaceMenu = () => {
               <Image src={createLogo} alt="create" className="h-[13px] w-auto"/>
               <p className="text-sm  cursor-pointer">Create</p>
             </Link>
-          </ul>
-          <button className="flex justify-start items-center gap-1 mt-2" onClick={()=>setIsShowInterviewCategory((prev)=>!prev)}>
-            <Image src={interviewLogo} alt="coursesLogo" className="h-[15px] w-auto"/>
-            <p className="hover:text-gray-300">Interviews</p>
-          </button>
-          <ul className={`pl-4 ${isShowInterviewCategory?'flex flex-col':'hidden'}  gap-1`}>
-            <li className="flex justify-start items-center gap-1 hover:text-gray-300">
-              <Image src={allLogo} alt="saved" className="h-[13px] w-auto"/>
-              <Link href={'/workspace/interviews/all'} className="text-sm  cursor-pointer">All</Link>
-            </li>
-            <li className="flex justify-start items-center gap-1 hover:text-gray-300">
-              <Image src={pendingLogo} alt="saved" className="h-[13px] w-auto"/>
-              <Link href={'/workspace/interviews/pending'} className="text-sm  cursor-pointer">Pending</Link>
-            </li>
-            <li className="flex justify-start items-center gap-1 hover:text-gray-300">
-              <Image src={completedLogo} alt="explore" className="h-[13px] w-auto"/>
-              <Link href={'/workspace/interviews/completed'} className="text-sm  cursor-pointer">Completed</Link>
-            </li>
-            <li className="flex justify-start items-center gap-1 hover:text-gray-300">
-              <Image src={expiredLogo} alt="expire" className="h-[13px] w-auto"/>
-              <Link href={'/workspace/interviews/expired'} className="text-sm  cursor-pointer">Expired</Link>
-            </li>
           </ul>
         </div>
       </div>
--- client/components/workspace/recruiters/jobs/my-jobs/applicants/ApplicantsCard.tsx
+++ client/components/workspace/recruiters/jobs/my-jobs/applicants/ApplicantsCard.tsx
@@ -1,8 +1,14 @@
-
+'use client'
+import { setPreviewedCandidate } from "@/redux/features/utils"
+import { useAppDispatch } from "@/redux/lib/hooks"
 import Image from "next/image"
 
 
 const ApplicantsCard = () => {
+    const dispatch = useAppDispatch()
+    const handleViewProfile = ()=>{
+        dispatch(setPreviewedCandidate(true))
+    }
   return (
     <div className='w-full h-[60px] shrink-0 flex justify-start items-center px-2 gap-3 text-[12px] text-gray-200 border-b-[1px] border-gray-700 pb-2 hover:border-blue-500 transition-colors duration-300 '>
         <div className="w-[40px] h-[40px] rounded-full ">
@@ -19,8 +25,8 @@ const ApplicantsCard = () => {
         <div className=" self-start mt-[-5px]  relative group">
             <p className="text-lg font-bold">...</p>
             <div className="absolute hidden group-hover:flex flex-col w-[200px] h-auto top-5 right-0 border border-gray-700 rounded-md z-10 bg-gray-900 backdrop-blur-2xl text-[10px] font-semibold">
-                <button className=" flex justify-center items-center w-full h-[30px] border-b-[1px] border-gray-700 hover:border-blue-600 cursor-pointer">
-                    View
+                <button onClick={handleViewProfile} className=" flex justify-center items-center w-full h-[30px] border-b-[1px] border-gray-700 hover:border-blue-600 cursor-pointer">
+                    View Profile
                 </button>
                 <button className=" flex justify-center items-center w-full h-[30px] border-b-[1px] border-gray-700 hover:border-teal-600 cursor-pointer">
                     Mark as Shortlisted
