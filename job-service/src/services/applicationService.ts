6521d2d004e8e5b9685f7d7c2b0a598f128a6635
Imran <imranbinazad777@gmail.com>
2025-09-08 17:46:43 +0600

    interview invitation tested

--- job-service/src/services/applicationService.ts
+++ job-service/src/services/applicationService.ts
@@ -1,11 +1,10 @@
 import { 
-  CreateJobRequestSchema, 
-  JobFilterSchema, 
-  ApiResponse, 
-  Pagination,
-  CreateJobRequest,
-  JobDetails
-} from '../types/job.types';
+  CandidateInfo,
+  InterviewData,
+} from '../types/aplication.types';
+import { asyncHandler } from '../utils';
+import axios from 'axios';
+
 // Status enum for job applications
 enum ApplicationStatus {
   Pending = 'PENDING',
@@ -34,7 +33,45 @@ class ApplicationService{
 
     return Promise.all(job);
   }
-    
+  
+  async sendInterviewInvitation(candidates: CandidateInfo[], jobId: string): Promise<void> {
+
+    const job = await JobDetailsModel.findById(jobId).orFail();
+   
+    for (const candidate of candidates) {
+      const response = await axios.post(`${process.env.INTERVIEW_SERVICE_URL}/api/interview`, {
+        candidate: {
+          id: candidate.candidateId,
+          email: candidate.candidateEmail,
+          name: candidate.candidateName,
+        },
+        job: {
+          id: job._id.toString(),
+          title: job.title,
+          interviewQA: job.interviewQA || [], // Assuming this field exists in your job model
+        },
+        deadline: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days from now
+      });
+      
+      const interview: InterviewData = response.data.data;
+      
+      const notification = {
+        type          : 'job.application.shortlisted',
+        candidateName : candidate.candidateName,
+        candidateEmail: candidate.candidateEmail,
+        jobTitle      : job.title,
+        OrganizationName : job.company.OrganizationName || " No name Found ",
+        OrganizationEmail: job.company.OrganizationEmail,
+        deadline         : interview.deadline,
+        guideLink        : 'https://github.com/Imran-2020331101',
+        interviewLink : `${process.env.FRONTEND_URL}/workspace/interviews/on-going/${interview.interviewId}`,
+      };
+      console.log(notification);
+
+      sendNotification(notification, "email-notification");
+    }
+  }
+
 }
 
 export const applicationService = new ApplicationService();
