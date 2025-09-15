fbb31d6e65142c15f96620b313c284230289eb11
Imran <imranbinazad777@gmail.com>
2025-09-15 20:27:11 +0600

    course service bug fixed

--- resume-service/src/services/courseService.ts
+++ resume-service/src/services/courseService.ts
@@ -1,4 +1,4 @@
-import { Course, ICourse, IThumbnails, SearchResult } from "../models/SavedCourseSchema";
+import { ICourse, IThumbnails, SearchResult } from "../models/SavedCourseSchema";
 import axios from "axios";
 
 class CourseService{
@@ -30,20 +30,6 @@ class CourseService{
         }
     }
 
-    async saveCourse( courseDetails : ICourse ) : Promise<ICourse>{
-        const course = new Course(courseDetails);
-        const savedCourse = await course.save();
-        return savedCourse as ICourse;
-    }
-
-    async searchSavedCourse( query: string ) : Promise<ICourse[]> {
-        return await Course.find({
-        $or: [
-            { title: { $regex: query, $options: "i" } },
-            { description: { $regex: query, $options: "i" } },
-        ],
-        });
-    }
 }
 
 export const courseService = new CourseService();
